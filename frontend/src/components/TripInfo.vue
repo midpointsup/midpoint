9c55d616163b323f90a1ecd3f2a479fd2502c0fe
MadisonMajarais <madison.majarais@gmail.com>
2024-07-11 09:41:20 -0400

    update backend models, routers, and add socket

--- frontend/src/components/TripInfo.vue
+++ frontend/src/components/TripInfo.vue
@@ -1,13 +1,20 @@
 <template>
-  <div></div>
+  <div>Trip info</div>
 </template>
 
 <script>
+import io from "socket.io-client";
+
 export default {
-  mounted() {
-    const socket = io();
+  created() {
+    const socket = io("http://localhost:3000");
+    
+    socket.on("connect", () => {
+      console.log("connected");
+    });
 
     socket.on("trip", (trip) => {
+      console.log("trip")
       console.log(trip);
     });
   },
